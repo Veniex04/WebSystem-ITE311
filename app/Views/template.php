2cce9a2de48e6675c11ff22489ad6e0b36c421ea
Veniex04 <joshuapecayo25@gmail.com>
2025-08-19 19:40:40 +0800

    updating index

--- app/Views/template.php
+++ app/Views/template.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
-  <title>Instagram-Themed LMS</title>
+  <title>LMS</title>
   <style>
     body {
       background-color: #f8f9fa;
@@ -19,7 +19,7 @@
     .nav-link,
     .dropdown-item {
       color: white !important;
-    }
+    }                                                                                                                                                                                                                                                                                                                                  
 
     .nav-link:hover,
     .dropdown-item:hover {
